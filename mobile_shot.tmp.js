2f499ca266b61dd6afba988b917d083e597e5c6d
siyante003 <muhammadciyam@gmail.com>
2026-07-31 12:45:27 +0500

    10 delete

--- mobile_shot.tmp.js
+++
@@ -1,9 +0,0 @@
-const { chromium } = require("playwright");
-(async () => {
-  const browser = await chromium.launch();
-  const page = await browser.newPage({ viewport: { width: 390, height: 850 } });
-  await page.goto("http://localhost:3000/");
-  await page.waitForLoadState("networkidle");
-  await page.screenshot({ path: process.argv[2] });
-  await browser.close();
-})();

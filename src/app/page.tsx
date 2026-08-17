92a2412f71e7eac9793ecf328ebe32b2e22c23f1
siyante003 <muhammadciyam@gmail.com>
2026-08-17 22:00:32 +0500

    sadad

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { Header } from "@/components/Header";
 import { Logo } from "@/components/Logo";
 import { CategorySidebar } from "@/components/CategorySidebar";
